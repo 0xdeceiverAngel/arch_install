517554b9a22b8196064a39ebed5963b80d5603d4
行者 <31663805+0xdeciverAngel@users.noreply.github.com>
2019-12-23 20:33:38 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,13 +7,6 @@ sudo pip3 install --upgrade git+https://github.com/arthaud/python3-pwntools.git
 sudo apt-get install zsh -y
 sudo apt-get install git -y
 sudo apt install curl -y
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-chsh -s /bin/zsh
-cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
-
-sudo apt-get install powerline -y
-sudo apt-get install fonts-powerline -y
-
 
 git clone https://github.com/longld/peda.git ~/peda
 echo "source ~/peda/peda.py" >> ~/.gdbinit
@@ -30,3 +23,12 @@ sudo apt-get install rar -y
 
 sudo apt-get install tmux -y
 sudo apt-get install vim -y
+
+sudo apt install default-jdk -y
+
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+chsh -s /bin/zsh
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+
+sudo apt-get install powerline -y
+sudo apt-get install fonts-powerline -y
