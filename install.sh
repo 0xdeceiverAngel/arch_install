e1291c8321f0e3303f9cbcc1b5d2cd8e352a60e8
行者 <31663805+0xdeciverAngel@users.noreply.github.com>
2020-07-30 10:01:37 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -47,3 +47,16 @@ sudo apt install nodejs -y
 
 sudo apt install npm -y
 sudo apt install nodejs-legacy -y
+
+sudo apt-get remove -y docker docker-engine docker.io containerd runc; \
+sudo apt-get update && \
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common && \
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable" && \
+sudo apt-get update && \
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+sudo apt install docker-compose -y
